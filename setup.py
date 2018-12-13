c0078fab3d86c7654d553aeca71c2c0a08d791d3
Zero <Zeroto521@gmail.com>
2018-12-13 20:25:56 +0800

    :bookmark: :sparkles: New version
    Using numba to improve proterty.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='dhnn',
-    version='0.1.4',
+    version='0.1.6',
     description='Discrete Hopfield Network (DHNN) implemented with Python',
     author='Zeroto521',
     author_email='Zeroto521@gmail.com',
     license="MIT",
     py_modules=['dhnn'],
-    requires=['numpy'],
-    install_requires=['numpy'],
+    requires=['numpy', 'numba'],
+    install_requires=['numpy', 'numba'],
     url='https://github.com/Zeroto521/DHNN',
     download_url='https://github.com/Zeroto521/DHNN/archive/master.zip',
     keywords=[
