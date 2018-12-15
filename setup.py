4e4e6522c76108dc81a9280cf77134fc3089edeb
Zero <Zeroto521@gmail.com>
2018-12-15 21:26:44 +0800

    :wrench: Update setup.py

--- setup.py
+++ setup.py
@@ -2,25 +2,28 @@ import setuptools
 
 from dhnn import __version__
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setuptools.setup(
     name='dhnn',
     version=__version__,
-    description='Discrete Hopfield Network (DHNN) implemented with Python',
+    description='A Discrete Hopfield Neural Network Framework in python',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='yosukekatada,Zeroto521',
     author_email='Zeroto521@gmail.com',
+    maintainer='Zeroto521',
+    maintainer_email='Zeroto521@gmail.com',
     license="MIT",
     py_modules=['dhnn'],
     requires=['numpy', 'numba'],
-    install_requires=['numpy', 'numba'],
+    install_requires=[],
     url='https://github.com/Zeroto521/DHNN',
     download_url='https://github.com/Zeroto521/DHNN/archive/master.zip',
-    python_requires=">=3",
+    python_requires=">=3.5",
     package_data={"": ["assets/*.jpg"]},
+    platforms=['linux', 'windows', 'macos'],
     keywords=[
         'machine learning',
         'neural networks',
