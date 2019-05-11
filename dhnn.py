fecb7224b9e8f912e748b5287254bf2188bd6be8
Zero <zeroto521@gmail.com>
2019-05-11 22:32:28 +0800

    :bug: flag no use

--- dhnn.py
+++ dhnn.py
@@ -23,14 +23,14 @@ import numba as nb
 import numpy as np
 
 
-__version__ = '0.1.9'
+__version__ = '0.1.10'
 __license__ = 'MIT'
 __short_description__ = 'A Discrete Hopfield Neural Network Framework in python.'
 
 
 class DHNN(object):
 
-    def __init__(self, isload=False, wpath='weigh.npy', pflag=1, nflag=-1):
+    def __init__(self, isload=False, wpath='weigh.npy', pflag=1, nflag=0):
         """Initializes DHNN.
 
         Keyword Arguments:
@@ -40,30 +40,17 @@ class DHNN(object):
             nflag {int} -- negative flag (default: -1)
         """
 
-        self.pflag = 1
-        self.nflag = -1
+        self.pflag = pflag
+        self.nflag = nflag
 
         if isload and os.path.isfile(wpath):
-            self.weight = np.load(wpath)
+            self._w = np.load(wpath)
         else:
-            self.weight = None
+            self._w = None
 
-    @nb.autojit
-    def create_W(self, data):
-        """Create network weight.
-
-        Arguments:
-            data {list} -- each sample is vector.
-
-        Returns:
-            np.ndarray -- matrix
-        """
-
-        mat = np.vstack(data)
-        eye = len(data) * np.identity(np.size(mat, 1))
-        weight = np.dot(mat.T, mat) - eye
-
-        return weight
+    @property
+    def weight(self):
+        return self._w
 
     @nb.autojit
     def train(self, data, issave=False, wpath='weigh.npy'):
@@ -77,11 +64,12 @@ class DHNN(object):
             wpath {str} -- the local weight path (default: {'weigh.npy'})
         """
 
-        if self.weight is None:
-            self.weight = self.create_W(data)
+        mat = np.vstack(data)
+        eye = len(data) * np.identity(np.size(mat, 1))
+        self._w = mat.T @ mat - eye
 
-            if issave:
-                np.save(wpath, self.weight)
+        if issave:
+            np.save(wpath, self.weight)
 
     @nb.autojit
     def predict(self, data, theta=0.5, epochs=1000):
@@ -98,9 +86,10 @@ class DHNN(object):
             np.ndarray -- recoveried sample
         """
 
-        indexs = np.random.randint(0, len(data)-1, epochs)
+        indexs = np.random.randint(0, len(data) - 1, epochs)
         for ind in indexs:
-            data[ind] = self.pflag if np.dot(
-                self.weight[ind], data) - theta > 0 else self.nflag
+            vec = self._w[ind]
+            value = vec.dot(data)
+            data[ind] = self.pflag if (value - theta > 0) else self.nflag
 
         return data
